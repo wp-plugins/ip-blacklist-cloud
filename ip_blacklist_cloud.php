4d6e759385f0483b4ff06d6764b2dc976d6b64b7
ad33lx <ad33lx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-11 23:23:33 +0000

    Version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/ip-blacklist-cloud/tags/1.3@637353 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ip_blacklist_cloud.php
@@ -0,0 +1,491 @@
+<?php
+/*
+Plugin Name: IP Blacklist Cloud
+Plugin URI: http://wordpress.org/extend/plugins/ip-blacklist-cloud/
+Description: Blacklist IP Addresses from visiting your WordPress website.
+Version: 1.3
+Author: Adeel Ahmed
+Author URI: http://demo.ip-finder.me/demo-details/
+*/
+
+
+function ip_added()
+{
+
+global $wpdb,$found,$IP_global, $IP_error;
+
+
+	$IP=$_GET['blacklist'];
+
+$IP_in_DP=$wpdb->get_var("SELECT id FROM ".$wpdb->prefix."IPBLC_blacklist WHERE IP='$IP'");
+
+	$found=false;
+	//$found=true;
+
+	if(!$IP_in_DP)
+	{
+
+
+
+		if(filter_var($IP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4))
+		{
+		
+
+		$table=$wpdb->prefix."IPBLC_blacklist";
+		$time=time();
+
+			$post_IP=$wpdb->insert( 
+				$table, 
+				array( 
+					'IP' => $IP, 
+					'timestamp' => $time
+				), 
+				array( 
+					'%s', 
+					'%d' 
+				) 
+			);
+
+//---post data to ip-finder.me
+$contextData = array ( 
+                'method' => 'POST',
+                'header' => "Connection: close\r\n". 
+             "Referer: ".site_url()."\r\n");
+ 
+// Create context resource for our request
+$context = stream_context_create (array ( 'http' => $contextData ));
+ 
+// Read page rendered as result of your POST request
+
+$link="http://ip-finder.me/wp-content/themes/ipfinder/blacklist_add.php?IP=$IP&website=".urlencode(site_url())."&website_name=".urlencode(get_bloginfo('name'));
+$post_to_cloud =  file_get_contents (
+                  $link,  // page url
+                  false,
+                  $context);
+
+
+
+		}
+		else
+		{
+			$found=true;
+			$IP_error=true;
+
+		}	
+	}
+	else
+	{
+		$found=true;
+	}
+
+
+$IP_global=$IP;
+
+}
+function ip_added_message()
+{
+global $found,$IP_global,$IP_error;
+
+$IP=$IP_global;
+
+
+	if(!$found)
+	{
+?>
+<style>
+#IPBLC_message_blacklist
+{
+	position: fixed;
+	bottom: 8px;
+	float: right;
+	right: 8px;
+	border:1px solid #99FF99;
+	background-color: #DDFFDD;
+	font-size: 20px;
+	//font-weight: bold;
+	padding: 12px;
+	color: #000000;
+
+}
+
+</style>
+<?php
+	
+
+	echo "<div id=\"IPBLC_message_blacklist\">$IP added to blacklist. Please comment on <a href=\"http://ip-finder.me/wpip?IP=$IP\" target=\"_blank\">IP-FINDER.ME</a></div>";
+
+	}
+	else
+	{
+
+?>
+<style>
+#IPBLC_message_blacklist
+{
+	position: fixed;
+	bottom: 8px;
+	float: right;
+	right: 8px;
+	border:1px solid #FF9999;
+	background-color: #FFDDDD;
+	font-size: 20px;
+	//font-weight: bold;
+	padding: 12px;
+	color: #000000;
+
+}
+
+</style>
+<?php
+	
+	
+	if(!$IP_error)
+	{
+		echo "<div id=\"IPBLC_message_blacklist\">$IP already added to blacklist.</div>";
+	}
+	else
+	{
+		echo "<div id=\"IPBLC_message_blacklist\">$IP is not valid IP Address.</div>";
+	}
+
+
+
+
+
+
+	}
+
+	echo "<script>jQuery('#IPBLC_message_blacklist').delay(8500).fadeOut(6000); jQuery('#IPBLC_message_blacklist').click(function(){ jQuery(this).hide(); });</script>";
+
+}
+
+function page_IPBLC_actions() {  
+	add_menu_page( "IP Blacklist", "IP Blacklist", "manage_options", "wp-IPBLC");
+	add_submenu_page( "wp-IPBLC", "Blacklist", "Blacklist", "manage_options", "wp-IPBLC", "blacklist_tool" );
+	add_submenu_page( "wp-IPBLC", "Add IP to Blacklist", "Add IP to Blacklist", "manage_options", "wp-IPBLC-Add", "blacklist_add" );
+	add_submenu_page( "wp-IPBLC", "Blacklist Statistics", "Blacklist Statistics", "manage_options", "wp-IPBLC-stats", "blacklist_stats" );
+} 
+
+
+
+function blacklist_tool() {
+
+	include "blacklist-list.php";
+
+}
+function blacklist_add() {
+
+	include "blacklist-add.php";
+
+}
+function blacklist_stats() {
+
+
+
+//---post data to ip-finder.me
+$contextData = array ( 
+                'method' => 'POST',
+                'header' => "Connection: close\r\n". 
+             "Referer: ".site_url()."\r\n");
+ 
+// Create context resource for our request
+$context = stream_context_create (array ( 'http' => $contextData ));
+ 
+// Read page rendered as result of your POST request
+
+$link="http://ip-finder.me/analysis";
+$analysis =  file_get_contents (
+                  $link,  // page url
+                  false,
+                  $context);
+
+
+
+echo $analysis;
+
+
+
+}
+
+
+function IPBLC_IP_column( $columns )
+{
+	$columns['IPBLC_IP'] = __( 'IP' );
+	$columns['IPBLC_IP_status'] = __( 'IP Staus' );
+	$columns['IPBLC_IP_spam'] = __( 'Spam Percentage' );
+	return $columns;
+}
+function IPBLC_IP_value( $column, $comment_ID )
+{
+
+global $wpdb;
+
+	if ( 'IPBLC_IP' == $column ) {
+
+		$IP=get_comment_author_IP($comment_ID);
+		echo "$IP";
+
+
+		echo '<div class="row-actions">
+	<span class="edit">
+		<a href="http://ip-finder.me/wpip?IP='.$IP.'" target="_blank" title="IP Details on IP-Finder.me">IP Details</a>
+	</span>
+	<span class="edit">
+		<a href="?blacklist='.$IP.'" title="Blacklist IP">Blacklist</a>
+	</span>
+</div>
+	'; //---echo end----
+
+	}
+	if ( 'IPBLC_IP_status' == $column ) {
+
+		$IP=get_comment_author_IP($comment_ID);
+
+
+		$IP_in_DP=$wpdb->get_var("SELECT id FROM ".$wpdb->prefix."IPBLC_blacklist WHERE IP='$IP'");
+
+		if($IP_in_DP)
+		{
+			echo "<font color=red><b>Blacklisted</b></font>";
+		}
+		else
+		{
+
+			echo "<font color=green><b>Neutral</b></font>";
+
+		}
+
+	}
+	if ( 'IPBLC_IP_spam' == $column ) {
+
+			echo "<div id=\"IPSpam-$comment_ID\" class=\"IPSpam\">N/A</div>";
+			echo "<a href=\"#\" id=\"IPSpamAction-$comment_ID\" class=\"IPSpamAction\" name=\"$comment_ID\">Calculate</a>";
+
+	}
+
+	
+}
+function create_sql()
+{
+global $wpdb;
+
+	if(!($wpdb->query("SELECT * FROM ".$wpdb->prefix."IPBLC_blacklist"))) 
+	{
+		$sql = "CREATE TABLE ".$wpdb->prefix."IPBLC_blacklist (
+		id INT(60) UNSIGNED NOT NULL AUTO_INCREMENT,
+		IP VARCHAR(200) NOT NULL, 
+		timestamp VARCHAR(500) NOT NULL, 
+		UNIQUE KEY id (id)
+		);";
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+
+
+
+
+	if($_GET['blacklist'])
+	{
+
+		//print_r($_SERVER);
+
+
+		$referer=$_SERVER['HTTP_REFERER'];
+		if(strpos($referer,"edit-comments.php")>0)
+		{
+
+			ip_added();
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+}
+
+
+function IPBLC_blockip()
+{
+global $wpdb;
+
+	$IP=$_SERVER['REMOTE_ADDR'];
+
+	$IP_in_DP=$wpdb->get_var("SELECT id FROM ".$wpdb->prefix."IPBLC_blacklist WHERE IP='$IP'");
+
+	if($IP_in_DP)
+	{
+	?>
+<head><title><?php echo get_bloginfo('name'); ?></title></head>
+
+<style>
+#IPBLC_message_blacklist
+{
+	border:1px solid #FF9999;
+	background-color: #FFDDDD;
+	font-size: 20px;
+	//font-weight: bold;
+	padding: 12px;
+	color: #000000;
+	margin-top: 50px;
+}
+</style>
+<center>
+<div id="IPBLC_message_blacklist">Your IP <?php echo $IP; ?> has been blacklisted!</div><BR>
+</center>
+
+	<?php
+		
+
+		exit();
+
+	}
+
+
+}
+
+$found=false;
+$IP_global="";
+$IP_error=false;
+
+add_action('admin_menu', 'page_IPBLC_actions');  
+add_action('admin_init', 'create_sql');  
+add_filter( 'manage_edit-comments_columns', 'IPBLC_IP_column' );
+add_filter( 'manage_comments_custom_column', 'IPBLC_IP_value', 10, 2 );
+
+add_action('init', 'IPBLC_blockIP');  
+
+
+function load_custom_IPBLC_admin_style() {
+        echo "<style type=\"text/css\">\n
+	.IPSpam, .IPSpamAction{ display: inline; margin: 0px 10px;}\n		
+	</style>\n
+	";
+
+}
+
+function IPJS()
+{
+
+
+
+if($_GET['blacklist'])
+{
+
+	//print_r($_SERVER);
+
+
+	$referer=$_SERVER['HTTP_REFERER'];
+	
+
+	if(strpos($referer,"edit-comments.php")>0)
+	{
+		ip_added_message();
+
+	}
+}
+
+
+
+
+
+$scriptname=$_SERVER['SCRIPT_NAME'];
+
+
+$page=$_GET['page'];
+
+
+if((strpos($scriptname,"edit-comments.php")>0) || (strpos($scriptname,"admin.php")>0 && ($page=="wp-IPBLC-stats" || $page=="wp-IPBLC" || $page=="wp-IPBLC-Add")) )
+{
+?>
+<style>
+#IPBLC_message_like
+{
+	position: fixed;
+	bottom: 20px;
+	float: left;
+	left: 5px;
+	border:1px solid #99AA99;
+	background-color: #EFEFEF;
+	font-size: 12px;
+	//font-weight: bold;
+	padding: 4px;
+	color: #000000;
+
+}
+#IPBLC_message_like a
+{
+	color: #222222;
+}
+</style>
+<div id="IPBLC_message_like">
+<a href="http://wordpress.org/extend/plugins/ip-blacklist-cloud/" target="_blank">Rate IP Blacklist Cloud</a>
+</div>
+<?php
+}
+?>
+
+
+
+
+<script type="text/javascript">
+
+jQuery(".IPSpamAction").click(function(){
+
+	var comment_ID=jQuery(this).attr('name');
+	var spamperc=jQuery("#IPSpam-"+comment_ID);
+
+	spamperc.css("color","#000000");
+	spamperc.html("<img src=\"<?php echo site_url(); ?>/wp-admin/images/wpspin_light.gif\">");
+
+
+
+<?php
+	//------plugin url---
+	$plugin_dir_name=plugin_dir_url(__FILE__);
+	//------SpamCheckerfile---
+	$SpamCheckerUrl=$plugin_dir_name."SpamChecker.php";
+
+
+?>
+
+		var reRequest=jQuery.ajax({
+			  type: "POST",
+			  url: "<?php echo $SpamCheckerUrl; ?>",
+			  data: {comment_ID: comment_ID},
+  			dataType: "html"
+			});
+
+		reRequest.done(function(msg,response) {
+			if(msg)
+			{
+			spamperc.css("color","#000000");
+			spamperc.html(msg);
+			}
+		});
+
+		reRequest.fail(function(jqXHR, textStatus) {
+			spamperc.css("color","#FF0000");
+			spamperc.html("<?php echo __("Error"); ?>");
+		});
+
+	return false;
+
+
+});
+
+</script>
+<?php
+
+
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_IPBLC_admin_style' );
+
+add_action('admin_footer', 'IPJS');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,111 @@
+=== IP Blacklist Cloud ===
+Contributors: ad33lx
+Donate link: 
+Tags: comments, spam, IP, blacklist, cloud, IP cloud, block, spamming, secure
+Requires at least: 3.3
+Tested up to: 3.4.2
+Stable tag: 1.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Blacklist IP Addresses from visiting your WordPress website.
+
+== Description ==
+
+IP Blacklist Cloud plugin allows you to blacklist IP addresses from visiting your WordPress website. Also, it submits your website link to the blocked IP database on [http://ip-finder.me](http://ip-finder.me/) which gives the other users to view how many sites have blocked the specific IP and see their comments.
+
+
+== Installation ==
+
+Note: Before downloading and installing plugin, you must accept that plugin sends your website url (using site_url()), website name (using get_bloginfo(‘name’)) automatically when you blacklist any IP to http://ip-finder.me and show this on relative blacklisted IP. (Example: http://ip-finder.me/wpip?IP=203.81.202.127)
+SpamChecker sends comments data (Name, Email, URL and Comment) to check Spam Percentages based on our database.
+
+
+1. Upload `ip_blacklist_cloud` folder to the `/wp-content/plugins/` directory
+
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== How Does it Work ==
+
+
+1. If you want to block an IP manually, you can do this by providing IP in admin menu “IP Blacklist->Add IP to Blacklist”.
+
+2. If any visitor or spamming bot post comment on your posts, by visiting “Comments” menu you can view IP and details on ip-finder.me or blacklist the IP.
+
+For example: http://ip-finder.me/wpip?IP=203.81.202.127
+
+3. Once you black list the IP address, the visitor will not be able to view any content of your website and also post your website link and name on IP Finder cloud.
+
+4. You can delete any IP from blacklist by visiting admin menu “IP Blacklist->Blacklist” and it will also remove your site link and name from the list of websites which have blocked that specific IP.
+
+
+5. You can leave comments on IP Cloud (example: http://ip-finder.me/wpip?IP=203.81.202.127 ) so that other users can get the idea why this IP have been blocked by your site.
+
+== Frequently asked questions ==
+
+Q1. What data does this plugin submits to http://ip-finder.me/
+
+Ans: * This plugin submits your WordPress site url and site name.
+     * Version 1.1 sends pending comment to http://ip-finder.me/ and find the same comment if it was posted on any other WordPress websites who have installed this plugin.
+     * Sending comment details to cloud requires user action and it is not automatic.
+
+
+Q2. Why does it sends WordPress site url and site name?
+
+Ans: In order to build this IP Cloud valuable to all the users, ip-finder.me save site name and site url only for specific IP (when you block it) so that it gives the attention to other users that this IP could be dangerous for their website.
+
+
+Q3. How and when does it sends data to ip-finder.me
+
+Ans: Plugin sends the data only on two conditions:
+  1. When you blacklist any IP on your website, it saves the data on ip-finder.me and add your site to the list of websites who have blocked that specific IP.
+  2. When you delete any IP from blacklist, it sends request to ip-finder.me to remove your site from the list of websites who have blocked that specific IP.
+  3. When you request to check Spam Percentage of any comment.
+
+Q4. If we block any IP, can they access example.com/wp-admin or still post comments?
+
+Ans: NO! This is the main reason why this plugin has been built to avoid spamming on WordPress based websites. you can test this on demo server (Please see Demo section below)
+
+
+Q6. What data does it sends for checking spam percentage of a comment?
+
+Ans: It sends Name, Email, URL and the contents of the comments.
+
+Q7. What data do you keep in your database?
+
+Ans: We keep details of 
+	* Blacklist IPs
+	* Websites' names and URLs who have blocked that specific IP address
+	* Comments details on which users have doubt that they are SPAM.
+
+
+== Screenshots ==
+
+1. http://demo.ip-finder.me/wp-content/uploads/2012/08/ip_box.png
+
+== DEMO ==
+
+You can test this plugin before downloading and installing on demo server.
+http://demo.ip-finder.me/demo-details/
+
+== Changelog ==
+
+= 1.3 =
+* Blacklist statistics page added in Admin Panel.
+
+= 1.2 =
+* Fixed Blacklist option on "Comments" Page for non-subdomain WordPress websites.
+
+= 1.1 =
+* Find Spam Pecentage of any pending comment by posting details to http://ip-finder.me/
+
+
+= 0.1 =
+* Initial Release
+
+
+== Upgrade Notice ==
+
+= 1.3 =
+* Blacklist statistics page added in Admin Panel.
