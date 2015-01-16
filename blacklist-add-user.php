b2578812207e6af4f91950c27fa32d484c8e520c
ad33lx <ad33lx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-16 12:15:19 +0000

    version 3.4 in tags only for getting approval
    git-svn-id: https://plugins.svn.wordpress.org/ip-blacklist-cloud/tags/3.4@1069279 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ blacklist-add-user.php
@@ -0,0 +1,113 @@
+<?php
+
+if ( !defined('ABSPATH') )
+    die ( 'No direct script access allowed' );
+
+?>
+<div class="wrap">
+<div id="icon-options-general" class="icon32"><br /></div>  
+<h2>Add Username to Blacklist</h2>
+<?php
+
+	global $wpdb,$USER_globale,$USER_error,$found;
+
+	if(isset($_POST['blacklist']))
+	{
+		$USER=$_POST['blacklist'];
+
+	$USER=urldecode($USER);
+	$USER=str_replace("\'","'",$USER);
+	$USER=str_replace("\\\'","'",$USER);
+	$USER=str_replace("\\\"",'\"',$USER);
+	$USER=str_replace("\\\"",'"',$USER);
+	$USER=str_replace("\"","&quot;",$USER);
+
+	$USER=mysql_real_escape_string($USER);
+
+
+		if($USER)
+		{
+
+ 			 // it's valid
+
+			$USER_in_DB=$wpdb->get_var("SELECT id FROM ".$wpdb->prefix."IPBLC_usernames WHERE USERNAME=\"$USER\"");
+
+			$found=false;
+			//$found=true;
+			if(!$USER_in_DB)
+			{
+
+				$table=$wpdb->prefix."IPBLC_usernames";
+				$time=time();
+
+				$wpdb->query("INSERT INTO $table (USERNAME,timestamp,visits,lastvisit) VALUES(\"$USER\",\"$time\",0,0)");
+				//$wpdb->print_error();
+
+				//---post blacklist data to ip-finder.me
+
+
+				$USER2=urlencode($USER);
+				post_blacklist_add_user($USER2);
+			}
+			else
+			{
+				$found=true;
+			}
+
+			if(!$found)
+			{
+			echo "<div id='setting-error-settings_updated' class='updated settings-error'>
+
+				<p><strong>$USER added to blacklist successfully!</strong></p></div>";
+			}
+			else
+			{
+			echo "<div id='setting-error-settings_updated' class='updated settings-error' style='color: #FF0000;'>
+
+				<p><strong>$USER already added to blacklist!</strong></p></div>";
+			}
+
+		}		
+	}
+
+?>
+<BR><BR>
+
+<form method=post>
+
+<table>
+
+<tr valign="top">
+
+<td>
+
+Username: 
+
+</td>
+
+<td>
+
+<input type=input name="blacklist" id="blacklist" value="" class="regular-text" style="width: 180px;">
+
+</td>
+
+</tr>
+
+
+
+<tr valign="top" valign="top">
+
+<td colspan=2 height=60>
+
+<input type="submit" name="add_user" id="add_user" value="Add User" class="button-primary">
+
+</td>
+
+</tr>
+
+</table>
+
+</form>
+
+</div>
+
---
+++ blacklist-add.php
@@ -0,0 +1,93 @@
+<?php
+
+if ( !defined('ABSPATH') )
+    die ( 'No direct script access allowed' );
+
+?>
+<div class="wrap">
+<div id="icon-options-general" class="icon32"><br /></div>  
+<h2>Add IP to Blacklist</h2>
+<BR>
+<B>NOTE:</B> After adding any IP to blacklist, please submit comment on IP-FINDER.ME to help others regarding the issue related to that specific IP.
+<BR>
+<?php
+
+	global $wpdb,$IP_globale,$IP_error,$found;
+
+	if(isset($_POST['blacklist']))
+	{
+		$IP=mysql_real_escape_string($_POST['blacklist']);
+
+		if(filter_var($IP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4))
+		{
+
+ 			 // it's valid
+			$IP_in_DP=$wpdb->get_var("SELECT id FROM ".$wpdb->prefix."IPBLC_blacklist WHERE IP=\"$IP\"");
+
+			$found=false;
+
+			//$found=true;
+
+			if(!$IP_in_DP)
+			{
+
+				$table=$wpdb->prefix."IPBLC_blacklist";
+				$time=time();
+
+				$wpdb->query("INSERT INTO $table (IP,timestamp,visits,lastvisit) VALUES(\"$IP\",\"$time\",0,0)");
+
+				//$wpdb->print_error();
+
+				//---post data to ip-finder.me
+				post_blacklist_add($IP);
+
+			}
+			else
+			{
+				$found=true;
+			}
+
+
+			if(!$found)
+			{
+			echo "<div id='setting-error-settings_updated' class='updated settings-error'>
+				<p><strong>$IP added to blacklist successfully!</strong></p></div>";
+			}
+			else
+			{
+			echo "<div id='setting-error-settings_updated' class='updated settings-error' style='color: #FF0000;'>
+				<p><strong>$IP already added to blacklist!</strong></p></div>";
+			}
+
+		}
+		else
+		{
+			  // it's not valid
+		echo "<div id='setting-error-settings_updated' class='updated settings-error'> 
+<p><strong>$IP is not valid IP address.</strong></p></div>";
+
+		}
+
+	}
+
+?>
+<BR><BR>
+<form method=post>
+<table>
+<tr valign="top">
+<td>
+IP: 
+</td>
+<td>
+<input type=input name="blacklist" id="blacklist" value="" class="regular-text" style="width: 180px;">
+</td>
+</tr>
+
+<tr valign="top" valign="top">
+<td colspan=2 height=60>
+<input type="submit" name="add_ip" id="add_ip" value="Add IP" class="button-primary">
+</td>
+</tr>
+</table>
+</form>
+</div>
